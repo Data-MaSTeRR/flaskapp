a7bfbee383a051f7fe46d4d9f11f0f0552fc699b
SKT001\SKT01 <hyunwoo8504@gmail.com>
2024-07-12 15:10:42 +0900

    update app2.py

--- app2.py
+++ app2.py
@@ -1,16 +1,9 @@
 # app.py
 from flask import Flask
-from flask import request
-
-import requests
-import re
-import socket
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_flask():
-    return "Hello, Webapp!"
+    return "Hello, Flask!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
